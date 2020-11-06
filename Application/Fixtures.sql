57de84a18bd681045a3405bd8a727fa8ba18ab0e
Pascal Engel <ip4ssi@gmail.com>
2020-11-06 10:43:39 +0100

    update fixtures

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -27,6 +27,7 @@ ALTER TABLE public.posts ENABLE TRIGGER ALL;
 ALTER TABLE public.comments DISABLE TRIGGER ALL;
 
 INSERT INTO public.comments (id, post_id, author, body, created_at, email) VALUES ('df634193-50ce-45ae-b8e2-1328e5a367a6', '6f33d32c-c254-4a69-8fa7-33f67908679b', 'Kevin314', 'How do you tame a horse in Minecraft??', '2020-07-31 12:43:48.644951+02', 'kev@gmail.com');
+INSERT INTO public.comments (id, post_id, author, body, created_at, email) VALUES ('05f007bc-b99d-42b3-bc57-92cbf85c5b2e', '6f33d32c-c254-4a69-8fa7-33f67908679b', 'Bilbo', 'Precious!!', '2020-11-06 10:28:25.514134+01', '');
 
 
 ALTER TABLE public.comments ENABLE TRIGGER ALL;
